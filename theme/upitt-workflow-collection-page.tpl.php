2a8b7cfb094adb3217d8c8d8494961eac77ccefa
Brian Gillingham <bgilling@pitt.edu>
2017-01-31 09:17:27 -0500

    ISLANDORA-101, added a collection manage form, fixed permissions, added an Audit tab for Islandora objects / manage

---
+++ theme/upitt-workflow-collection-page.tpl.php
@@ -0,0 +1,6 @@
+<?php include_once('upitt-workflow-breadcrumb.tpl.php'); ?>
+<?php include_once('upitt-workflow-admin-links.tpl.php'); ?>
+<h5><?php print $count_rows; ?></h5>
+<div style="overflow-x:auto" class="clearfix">
+  <?php print $table; ?>
+</div>
