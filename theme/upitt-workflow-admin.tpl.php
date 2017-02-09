c64236b9d7768d8fb6b9a6d09ee9b3bc0eb06c81
Brian Gillingham <bgilling@pitt.edu>
2017-02-09 09:13:47 -0500

    ISLANDORA-101 added some initial structure for supporting islandora_job (gearman) calls, added auto-create METS from item_files and edit with xonomy js editor

--- theme/upitt-workflow-admin.tpl.php
+++ theme/upitt-workflow-admin.tpl.php
@@ -7,17 +7,6 @@
 
 <?php include_once('upitt-workflow-admin-links.tpl.php'); ?>
 
-<h3>Collections Needing Rebuilding</h3>
-<ul>        
-  <li>pitttextdir</li>
-  <li>pittpress</li>
-  <li>pittmiscpubs</li>
-  <li>pittchancellor</li>
-  <li>pitttext</li>
-  <li>ulstext</li>          
-</ul>
-<i style="font-size:smaller">these are hard-coded in the template for now </i>
-
 
 <h3>Tasks</h3>
 <ul>
@@ -25,5 +14,4 @@
   <li class="big"><a href="/workflow/collection_building/">[online] Collection Building</a></li>
   <li class="big"><a href="/workflow/status/change/">Quick Status Change</a></li>
 </ul> 
-<i style="font-size:smaller">these are hard-coded in the template for now </i>
 
