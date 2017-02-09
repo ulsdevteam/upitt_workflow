bc904b1f17e72af4ffa00e5d010730529567c1d0
Brian Gillingham <bgilling@pitt.edu>
2017-02-09 15:19:17 -0500

    ISLANDORA-101 added code to update lookup list records, small bugfix in upitt_workflow_get_table_record, added link to create islandora collection, adjusted workflow/admin page

--- theme/upitt-workflow-admin.tpl.php
+++ theme/upitt-workflow-admin.tpl.php
@@ -1,17 +1,13 @@
 <?php include_once('upitt-workflow-breadcrumb.tpl.php'); ?>
 <?php include_once('upitt-workflow-findobject-form.tpl.php'); ?>
 
-<div style="overflow-x:auto">
-  <?php print $markup; ?>
-</div>
-
-<?php include_once('upitt-workflow-admin-links.tpl.php'); ?>
-
-
 <h3>Tasks</h3>
 <ul>
-  <li class="big"><a href="/workflow/batch/new/">Create New Batch</a></li>
-  <li class="big"><a href="/workflow/collection_building/">[online] Collection Building</a></li>
-  <li class="big"><a href="/workflow/status/change/">Quick Status Change</a></li>
+  <?php foreach ($task_links as $task_link): ?>
+    <li><?php echo $task_link; ?></li>
+  <?php endforeach; ?>
 </ul> 
+
+<h3>Edit Lists</h3>
+<?php include_once('upitt-workflow-admin-links.tpl.php'); ?>
 
