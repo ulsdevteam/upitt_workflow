a4a7d418d8597e9158fdb473b8ee91eef82e96b2
Brian Gillingham <bgilling@pitt.edu>
2017-06-08 17:22:29 -0400

    work in progress

--- js/workflow.js
+++ js/workflow.js
@@ -4,15 +4,10 @@
  */
 
 function host_selected(control, site_url) {
-  var ftp_selected;
-  // 
-  var initial_path;
-  initial_path = jQuery('#edit-initial-batch-host').val();
+  var ftp_selected = (control.value == 'ftp');
+  var initial_path = jQuery('#edit-initial-batch-host').val();
   jQuery('#edit-box-selector option').remove();
-  jQuery('#edit-box-selector').append('<option value="foo" selected="selected">Foo</option>');
-  jQuery('#edit-box-selector').append('<option value="bar" selected="selected">BAaaar</option>');
 
-  ftp_selected = (control.value == 'ftp');
   if (ftp_selected) {
     jQuery('#edit-local-files').hide();
     jQuery('#edit-box-files').show();
@@ -21,11 +16,7 @@ function host_selected(control, site_url) {
     jQuery('#edit-box-files').hide();
     jQuery('#edit-local-files').show();
   } 
-  /* var test_string = jQuery.ajax({
-  url: 'http://infost01-02.library.pitt.edu:8000/workflow/batch/edit/1429',
-  context: document.body,
-  });*/
-  var current_path = jQuery('[name="initial_batch_host"]').val();
+  var current_path = jQuery('[name="initial_ftp_path"]').val();
   current_path = current_path.replace("/", "|");
   
   var get_ftp_files_url = site_url + '/ajax/workflow/browse_ftp/' + current_path;
