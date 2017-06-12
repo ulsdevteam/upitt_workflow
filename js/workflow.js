343971c899f558af84712ec37201bb6cddd6deb7
Brian Gillingham <bgilling@pitt.edu>
2017-06-12 18:57:58 +0000

    made the ftp brower more user-friendly by adding loading animation and file / folder glyphs

--- js/workflow.js
+++ js/workflow.js
@@ -14,36 +14,29 @@ jQuery( document ).ready(function() {
 
 function host_selected(control, site_url) {
   var ftp_selected = (control == 'ftp' || control.value == 'ftp');
-  var initial_path = jQuery('#edit-initial-batch-host').val();
-  jQuery('#edit-box-selector option').remove();
-
-  jQuery('#edit-loading').val('...');
 
   if (ftp_selected) {
     jQuery('#edit-local-files').hide();
     jQuery('#edit-box-files').show();
+    var current_path = jQuery('[name="initial_ftp_path"]').val();
+    current_path = current_path.replace("ftp:", "");
+    ftp_change_path(current_path, site_url);
   }
   else {
     jQuery('#edit-box-files').hide();
     jQuery('#edit-local-files').show();
   } 
-  var current_path = jQuery('[name="initial_ftp_path"]').val();  
-  var get_ftp_files_url = site_url + '/ajax/workflow/browse_ftp/' + current_path;
-
-  jQuery('#edit-loading').val('done!');
-  
-  var ajaxcall = jQuery.get(get_ftp_files_url, function( data ) {
-    jQuery('#edit-box-files').html( data );
-  });
-          
 }
 
 function ftp_change_path(path, site_url) {
   var get_ftp_files_url = site_url + '/ajax/workflow/browse_ftp/' + path;
+  jQuery('#box-files-output').remove();
+  jQuery('#edit-box-files').html('<div id="edit-box-files-loading-img"><h3>Loading results from FTP</h3><p>Please be patient...</p></div>');
   var ajaxcall = jQuery.get(get_ftp_files_url, function( data ) {
     jQuery('#edit-box-files').html( data );
     path = path.replace("ftp:", "");
     jQuery('[name="initial_ftp_path"]').val('ftp:' + path);
+    jQuery('#edit-box-files').show();
   });
 }
 
